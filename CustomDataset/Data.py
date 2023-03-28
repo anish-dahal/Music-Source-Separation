feaf7f5ee4b547c831dc5491e53aa998a1524970
anish.dahal <anish.dahal@tai.com.npssh-keygen -t rsa -P  -m PEMgit clone AL-ML>
2023-03-28 11:14:09 +0545

    Convert audio file to spectrogram and save in .npz file and make custome dateset

---
+++ CustomDataset/Data.py
@@ -0,0 +1,38 @@
+import os
+
+import numpy as np
+import torch
+from torch.utils.data import DataLoader, Dataset, random_split
+
+torch.manual_seed(100)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+class DSD100(Dataset):
+    def __init__(self, data_path):
+        self.spectrogram_path = [data_path + '/'+name for name in sorted(os.listdir(data_path))]
+    
+    def __len__(self):
+        return len(self.spectrogram_path)
+    
+    def __getitem__(self, index):
+        data = np.load(self.spectrogram_path[index])
+        mixture = data['mixture'][np.newaxis,:,:]
+        bass = data['bass'][np.newaxis,:,:]
+        drum = data['drum'][np.newaxis,:,:]
+        vocal = data['vocal'][np.newaxis,:,:]
+        instrumental = data['instrumental'][np.newaxis,:,:]
+        return (
+            torch.from_numpy(np.copy(mixture)).to(device),
+            torch.from_numpy(np.copy(bass)).to(device),
+            torch.from_numpy(np.copy(drum)).to(device),
+            torch.from_numpy(np.copy(vocal)).to(device),
+            torch.from_numpy(np.copy(instrumental)).to(device)
+        )
+
+
+def data_split(dataset, train_ratio = 0.8):
+    return random_split(dataset, [train_ratio, 1 - train_ratio])
+
+
+def dataloader(dataset, batch_size = 64, shuffle = False):
+    return DataLoader(dataset, batch_size = batch_size, shuffle = shuffle)

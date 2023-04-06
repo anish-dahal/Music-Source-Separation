d73235efccee99cef35d6f2d9eff9e0fb6a98ae0
anish.dahal <anish.dahal@tai.com.npssh-keygen -t rsa -P  -m PEMgit clone AL-ML>
2023-04-06 10:57:58 +0545

    make demo.ipynb file and write some docstring

--- README.md
+++ README.md
@@ -2,8 +2,10 @@
 Project Title: Data-Driven Approach in Isolating Vocals and Instruments from Music <br />
 Dataset Used: DSD100 Dataset <br />
 link: https://sigsep.github.io/datasets/dsd100.html <br />
-This is a source separation project which separates vocals and instrumentals from a song.<br /> 
+This is a source separation project which separates vocals, instrumentals, bass and drum stem from a song.<br /> 
 U-net was used to train the model.<br />
 paper link: https://ejhumphrey.com/assets/pdf/jansson2017singing.pdf <br /> <br />
 Start Date: May 2021 <br />
-End Date: March 2022
+End Date: March 2022 <br /> <br />
+Result:
+The model provides satisfaction while separating the vocal and drum stems as compared to the other 2 stems.
--- model/Network.py
+++ model/Network.py
@@ -5,13 +5,16 @@ import torch.nn.functional as F
 
 torch.manual_seed(100)
 
+
 class encoder(nn.Module):
     def __init__(self, in_channel, out_channel):
         super().__init__()
-        self.conv = nn.Conv2d(in_channel, out_channel, kernel_size = (5, 5), stride = 2, padding = 1)
+        self.conv = nn.Conv2d(
+            in_channel, out_channel, kernel_size=(5, 5), stride=2, padding=1
+        )
         self.batchnorm = nn.BatchNorm2d(out_channel)
         self.l_relu = nn.LeakyReLU(0.2)
-    
+
     def forward(self, x):
         x = self.conv(x)
         x = self.batchnorm(x)
@@ -22,10 +25,11 @@ class encoder(nn.Module):
 class decoder(nn.Module):
     def __init__(self, in_channel, out_channel):
         super().__init__()
-        self.dconv = nn.ConvTranspose2d(in_channel, out_channel, kernel_size = (5, 5), stride = 2, padding = 1)
+        self.dconv = nn.ConvTranspose2d(
+            in_channel, out_channel, kernel_size=(5, 5), stride=2, padding=1
+        )
         self.batchnorm = nn.BatchNorm2d(out_channel)
-        
-    
+
     def forward(self, x):
         x = self.dconv(x)
         x = self.batchnorm(x)
@@ -45,23 +49,23 @@ class UNet(nn.Module):
         self.decoder1 = decoder(512, 256)
         self.dropout1 = nn.Dropout(0.5)
         self.relu1 = nn.ReLU()
-        self.decoder2 = decoder(256*2, 128)
+        self.decoder2 = decoder(256 * 2, 128)
         self.dropout2 = nn.Dropout(0.5)
         self.relu2 = nn.ReLU()
 
-        self.decoder3 = decoder(128*2, 64)
+        self.decoder3 = decoder(128 * 2, 64)
         self.dropout3 = nn.Dropout(0.5)
         self.relu3 = nn.ReLU()
 
-        self.decoder4 = decoder(64*2, 32)
+        self.decoder4 = decoder(64 * 2, 32)
         self.relu4 = nn.ReLU()
 
-        self.decoder5 = decoder(32*2, 16)
+        self.decoder5 = decoder(32 * 2, 16)
         self.relu5 = nn.ReLU()
 
-        self.decoder6 = decoder(16*2, 1)
+        self.decoder6 = decoder(16 * 2, 1)
         self.sigmoid = nn.Sigmoid()
-    
+
     def forward(self, x):
         conv1 = self.encoder1(x)
         conv2 = self.encoder2(conv1)
@@ -73,28 +77,27 @@ class UNet(nn.Module):
         dconv1 = self.decoder1(conv6)
         dconv1 = self.dropout1(dconv1)
         dconv1 = self.relu1(dconv1)
-        dconv1 = torch.concat([dconv1, conv5], dim = 1)
+        dconv1 = torch.concat([dconv1, conv5], dim=1)
 
         dconv2 = self.decoder2(dconv1)
         dconv2 = self.dropout3(dconv2)
         dconv2 = self.relu2(dconv2)
-        dconv2 = torch.concat([dconv2, conv4], dim = 1)
+        dconv2 = torch.concat([dconv2, conv4], dim=1)
 
         dconv3 = self.decoder3(dconv2)
         dconv3 = self.dropout3(dconv3)
         dconv3 = self.relu3(dconv3)
-        dconv3 = torch.concat([dconv3, conv3], dim = 1)
+        dconv3 = torch.concat([dconv3, conv3], dim=1)
 
         dconv4 = self.decoder4(dconv3)
         dconv4 = self.relu4(dconv4)
-        dconv4 = torch.concat([dconv4, conv2], dim = 1)
+        dconv4 = torch.concat([dconv4, conv2], dim=1)
 
         dconv5 = self.decoder5(dconv4)
         dconv5 = self.relu5(dconv5)
 
-        dconv5 = torch.concat([dconv5, conv1], dim = 1)
+        dconv5 = torch.concat([dconv5, conv1], dim=1)
 
         output = self.decoder6(dconv5)
         output = self.sigmoid(output)
         return output
-        
